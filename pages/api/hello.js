c8657f5472380ed6cbaa8b05dbb48e60a955808a
jackday1 <jack.nguyen@weareday.one>
2022-07-28 17:02:37 +0700

    add api

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,17 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
+let users = [{ name: 'John Doe' }];
+
 export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+  const { method } = req;
+
+  switch (method) {
+    case 'GET':
+      return res.status(200).json(users);
+    case 'POST':
+      users = [...users, { name: 'New guide' }];
+      return res.status(200).json(users);
+    default:
+      return res.status(400).send('Error');
+  }
 }
